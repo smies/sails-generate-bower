e083c84954a67a280ecd57e12c6362108cd2a355
James Smith <james.smith@cantab.net>
2014-05-01 18:45:59 +0100

    bug fixes

--- bin/index.js
+++ bin/index.js
@@ -19,10 +19,8 @@ var scope = {
 	modules: {
 		'bower': path.resolve(__dirname, '../lib')
 	},
-
-	// For the NEW generator we're generating:
-	//generatorName: process.argv[2],
 };
+
 sailsgen(scope, function (err) {
 	if (err) throw err;
 
